bea1e4b40ed386471f626c64834c339d8016d47c
thethawhtetzin05 <thawhtetzin05@gmail.com>
2026-08-17 16:24:55 +0630

    fix: make local config plugin safe for EAS read app config phase

--- mobile/plugins/withAndroidABIFilter.js
+++ mobile/plugins/withAndroidABIFilter.js
@@ -1,4 +1,10 @@
-const { withAppBuildGradle } = require('@expo/config-plugins');
+let withAppBuildGradle;
+try {
+  withAppBuildGradle = require('@expo/config-plugins').withAppBuildGradle;
+} catch (e) {
+  // Mock for EAS control plane which reads config before npm install
+  withAppBuildGradle = (config) => config;
+}
 
 module.exports = function withAndroidABIFilter(config) {
   return withAppBuildGradle(config, (config) => {
